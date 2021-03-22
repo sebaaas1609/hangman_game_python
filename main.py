9e242c2b2ee9a2710d9082cb299a2dd226407268
Sebas Granizo <sebasgranizo@MacBook-Pro-de-Sebas.local>
2021-03-22 10:58:27 -0500

    Completed project

---
+++ main.py
@@ -0,0 +1,97 @@
+import random
+
+from test import word_list
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+chosen_word = random.choice(word_list)
+display = []
+for _ in range(len(chosen_word)):
+    display += "-"
+
+end_of_game = False
+
+lives = 6
+print(''' _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/                       ''')
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    for position in range(len(chosen_word)):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+    if guess not in chosen_word:
+        lives -= 1
+    print(stages[lives])
+    print(f"{' '.join(display)}")
+
+    if "-" not in display:
+        end_of_game = True
+        print("You Win")
+    if lives == 0:
+        end_of_game = True
+        print("You Lose")
+        print(f'The word was {chosen_word}')
